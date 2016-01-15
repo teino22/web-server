65a81ed7b6928a0ec3da7a7028e432c6a473ecd3
Matteo Tegnenti <matteo.games2@gmail.com>
2016-01-15 13:45:00 +0100

    Update About us!

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var middleware = {
 app.use(middleware.logger);
 
 app.get('/about',middleware.requireAuthentication ,function (req, res){
-	res.send('About us');
+	res.send('About us!');
 });
 
 app.use(express.static(__dirname+'/public'));//expose an entire folder
